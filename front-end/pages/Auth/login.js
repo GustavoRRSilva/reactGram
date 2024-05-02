9c178e788aa40240a01722de49086a1d70ce4eca
Gustavo <gstvrrds@gmail.com>
2024-05-02 20:23:22 -0300

    feat:iniciando o front-end

---
+++ front-end/pages/Auth/login.js
@@ -0,0 +1,6 @@
+import styles from "@/styles/Auth.module.css"
+export default function Login(second) {
+  return <div>
+    <h1>Hello</h1>
+  </div>;
+}
---
+++ front-end/pages/Auth/register.js
@@ -0,0 +1,6 @@
+import styles from "@/styles/Auth.module.css"
+export default function Register(second) {
+  return <div className={styles.div}>
+    <h1>Hello</h1>
+  </div>;
+}
